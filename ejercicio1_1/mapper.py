47372bd53f8ad6d7db2818d652ebd0977c104f03
OliNaya <olinaya25@gmail.com>
2022-01-13 16:30:46 +0100

    ejercicio1

---
+++ ejercicio1_1/mapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 6:
+      continue
+    date, time, store, item, cost, payment = data
+    print(f'{store}\t{cost}')
